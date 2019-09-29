6c9f37a798d5143013d40d613e4c8045db379c18
Jonah Brown <jswish02@gmail.com>
2019-09-29 21:16:29 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,10 +1,10 @@
 class Mp3importer
-  attr_accessor :name
+  attr_accessor :path
   
   
   @@all = []
-  def initialize(name)
-  @name = name
+  def initialize(path)
+  @path = path
   @@all << self 
 end 
 def save
